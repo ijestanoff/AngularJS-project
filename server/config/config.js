2de167d68490742178ebf89a026cb56733eccf45
Iliyan Dzhestanov <jestanoff@icloud.com>
2024-12-19 19:42:21 +0200

    server set

--- server/config/config.js
+++ server/config/config.js
@@ -3,8 +3,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        //dbURL: 'mongodb://localhost:27017/forum',
-        dbURL: 'mongodb://192.168.1.153:27017/forum',
+        dbURL: 'mongodb://localhost:27017/forum',
+        //dbURL: 'mongodb://192.168.1.153:27017/forum',
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
