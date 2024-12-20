52394f20e1f9ceffa393d95a8d266689901dfb02
Iliyan Dzhestanov <jestanoff@icloud.com>
2024-12-20 17:57:53 +0200

    add 404 error component

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { PostsListComponent } from "../posts-list/posts-list.component";
 import { ThemesListComponent } from "../theme/themes-list/themes-list.component";
 import { UserService } from '../user/user.service';
-// import { HomeComponent } from "../home/home.component";
 
 @Component({
   selector: 'app-main',
